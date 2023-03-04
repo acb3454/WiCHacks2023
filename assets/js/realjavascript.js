826aa203a29c2653570b252e5c86bc0131dacea0
Aemilia <acb3454@rit.edu>
2023-03-04 15:24:18 -0500

    hopefully fixed checkbox label things

--- assets/js/realjavascript.js
+++ assets/js/realjavascript.js
@@ -2,15 +2,19 @@ function addTask() {
     //gets the task input value
     task = document.getElementById('task1').value;
     console.log("got new task " + task);
+    
     //put task in text node
-    newtask = document.createTextNode(task);
-    console.log("made text node of new task " + newtask);
+    const newtask = document.createElement("label");
+    newtask.innerHTML = task;
+    console.log("made label of task " + newtask);
+
     //make checkbox element
     const c = document.createElement("INPUT");
     c.setAttribute("type", "checkbox");
-    c.setAttribute("name", task);
+    c.setAttribute("id", "task");
     console.log("added checkbox of new task " + c);
     c.appendChild(newtask);
+
     //put current task on the list
     const current_task = document.getElementById('task-input-things');
     current_task.appendChild(c);
