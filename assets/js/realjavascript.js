1da469a38e5a473f28958694635824e8aedf73dc
emma1-edgar1 <95726955+emma1-edgar1@users.noreply.github.com>
2023-03-04 15:48:29 -0500

    smooth resize navbar

--- assets/js/realjavascript.js
+++ assets/js/realjavascript.js
@@ -22,3 +22,15 @@ function addTask() {
     console.log("appended task as child in input");
     document.getElementById('task1').value = '';
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
