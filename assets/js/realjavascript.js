378dba830d1764b5d3fb88b07368e0c94fc362cd
Aemilia <acb3454@rit.edu>
2023-03-04 15:08:21 -0500

    changed paragraph to checkbox

--- assets/js/realjavascript.js
+++ assets/js/realjavascript.js
@@ -3,8 +3,9 @@ function addTask() {
     console.log("got new task" + task);
     newtask = document.createTextNode(task);
     console.log("made text node of new task" + newtask);
-    const p = document.createElement("p");
-    console.log("added paragraph of new task" + p);
+    const p = document.createElement("INPUT");
+    p.setAttribute("type", "checkbox");
+    console.log("added checkbos of new task" + p);
     p.appendChild(newtask);
     const current_task = document.getElementById('task-input-things');
     current_task.appendChild(p);
