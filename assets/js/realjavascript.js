f4770d986e26137802e6755820e545c51df1eaab
Aemilia <acb3454@rit.edu>
2023-03-04 15:15:01 -0500

    testing more checkbox things

--- assets/js/realjavascript.js
+++ assets/js/realjavascript.js
@@ -1,13 +1,17 @@
 function addTask() {
+    //gets the task input value
     task = document.getElementById('task1').value;
-    console.log("got new task" + task);
+    console.log("got new task " + task);
+    //put task in text node
     newtask = document.createTextNode(task);
-    console.log("made text node of new task" + newtask);
+    console.log("made text node of new task " + newtask);
+    //make checkbox element
     const c = document.createElement("INPUT");
     c.setAttribute("type", "checkbox");
-    c.setAttribute("label", newtask);
-    console.log("added checkbox of new task" + p);
+    c.setAttribute("name", newtask);
+    console.log("added checkbox of new task " + c);
     c.appendChild(newtask);
+    //put current task on the list
     const current_task = document.getElementById('task-input-things');
     current_task.appendChild(c);
     console.log("appended task as child in input");
