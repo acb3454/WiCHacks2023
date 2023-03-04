8c3c2a076e144dbaac3bc0cd5a9a1cf127f8bf7f
Aemilia <acb3454@rit.edu>
2023-03-04 15:10:52 -0500

    checkbox changes

--- assets/js/realjavascript.js
+++ assets/js/realjavascript.js
@@ -3,12 +3,13 @@ function addTask() {
     console.log("got new task" + task);
     newtask = document.createTextNode(task);
     console.log("made text node of new task" + newtask);
-    const p = document.createElement("INPUT");
-    p.setAttribute("type", "checkbox");
-    console.log("added checkbos of new task" + p);
-    p.appendChild(newtask);
+    const c = document.createElement("INPUT");
+    c.setAttribute("type", "checkbox");
+    c.setAttribute("label", newtask);
+    console.log("added checkbox of new task" + p);
+    //c.appendChild(newtask);
     const current_task = document.getElementById('task-input-things');
-    current_task.appendChild(p);
+    current_task.appendChild(c);
     console.log("appended task as child in input");
     document.getElementById('task1').value = '';
 }
