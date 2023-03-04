7c232d978bdd04b1dadb3db48e17e398cd8bb13e
Aemilia <acb3454@rit.edu>
2023-03-04 14:33:26 -0500

    added logs to addtask

--- assets/js/realjavascript.js
+++ assets/js/realjavascript.js
@@ -1,8 +1,12 @@
 function addTask() {
-    task = document.getElementById('task1').valueOf;
+    task = document.getElementById('task1').value;
+    console.log("got new task");
     newtask = document.createTextNode(task);
+    console.log("made text node of new task");
     const p = document.createElement("p");
+    console.log("added paragraph of new task");
     p.appendChild(newtask);
     const current_task = document.getElementById('task-input-things');
     current_task.appendChild(p);
+    console.log("appended task as child in input");
 }
